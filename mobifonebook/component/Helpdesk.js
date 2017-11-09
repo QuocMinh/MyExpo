a2ccf96cf00d16ff4709f9d9371e65997ea53874
unknown <QuocMinh-PC@CTY9-CNTT-MINH.mobifone.vn>
2017-11-09 15:28:15 +0700

    Sound OK

--- mobifonebook/component/Helpdesk.js
+++ mobifonebook/component/Helpdesk.js
@@ -9,11 +9,14 @@ import {
     RefreshControl,
     TouchableOpacity
 } from 'react-native';
+import { Audio } from "expo";
 
-import Box      from './Box.js';
-import Config   from "../util/Config.js";
+import Box       from './Box.js';
+import Config    from "../util/Config.js";
+import AudioLink from "../util/AudioLink.js";
 
 const { width } = Dimensions.get("window");
+
 var dataArray = [
     { soquay: 1, trangthai: 0 },
     { soquay: 2, trangthai: 0 },
@@ -21,7 +24,7 @@ var dataArray = [
     { soquay: 4, trangthai: 0 },
     { soquay: 5, trangthai: 0 },
     { soquay: 6, trangthai: 0 }
-]
+];
 
 class Helpdesk extends Component {
     constructor(props) {
@@ -54,8 +57,17 @@ class Helpdesk extends Component {
                     <ListView
                         dataSource={this.state.dataSource}
                         renderRow={ (r) =>
-                            <TouchableOpacity onLongPress={() => { this.updateHelpdeskStatus(r.id, r.soquay) }} >
-                                <Box soquay={r.soquay} trangthai={r.trangthai} stt={r.stt} id={r.id} updateHelpdeskStatus={this.updateHelpdeskStatus} />
+                            <TouchableOpacity 
+                                onLongPress={() => { this.updateHelpdeskStatus(r.id, r.soquay) }}
+                                onPress={() => this.playSound(r.stt, r.soquay)}
+                            >
+                                <Box 
+                                    soquay={r.soquay} 
+                                    trangthai={r.trangthai} 
+                                    stt={r.stt} 
+                                    id={r.id} 
+                                    updateHelpdeskStatus={this.updateHelpdeskStatus}
+                                />
                             </TouchableOpacity>
                         }
                         contentContainerStyle={{ flexDirection: "row", flexWrap: "wrap" }}
@@ -70,6 +82,46 @@ class Helpdesk extends Component {
                 </View>
             </View>
         );
+    }
+
+    playSound = async(stt, soquay) => {
+        console.log("---- PLAY SOUND !!");
+        if(stt > 0) {
+            var sound1      = new Audio.Sound();
+            var sound2      = new Audio.Sound();
+            var soundStt    = new Audio.Sound();
+            var soundQuay   = new Audio.Sound();
+
+            try {
+                // Play sound 1
+                await sound1    .loadAsync(require("../assets/sounds/read-1.mp3"));
+                await sound2    .loadAsync(require("../assets/sounds/read-2.mp3"));
+                await soundStt  .loadAsync(AudioLink[stt - 1]);
+                await soundQuay .loadAsync(AudioLink[soquay - 1]);
+
+                await sound1.playAsync();
+                setTimeout(async() =>  { await soundStt   .playAsync(); }, 1850);
+                setTimeout(async() =>  { await sound2     .playAsync(); }, 2700);
+                setTimeout(async() =>  { await soundQuay  .playAsync(); }, 3700);
+
+                console.log("Phai play chu!!!!!");
+            } catch (error) {
+                console.log("ERROR: ");
+                console.log(error);
+            }
+        } else {
+            var soundDefault = new Audio.Sound();
+            try {
+                // Play sound 1
+                await soundDefault.loadAsync(require("../assets/sounds/default-voice.mp3"));
+                await soundDefault.playAsync();
+            } catch (error) {
+                console.log("ERROR: ");
+                console.log(error);
+            }
+            console.log("Khong hop le!");
+        }
+        
     }
 
     updateHelpdeskStatus(id, soquay) {
@@ -117,9 +169,7 @@ class Helpdesk extends Component {
             })
             .catch(err => {
                 console.log(err)
-                this.setState({
-                    refreshing: false
-                });
+                this.setState({ refreshing: false });
             });
     }
 
@@ -129,11 +179,7 @@ class Helpdesk extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexGrow: 1,
-        alignItems: 'center',
-        backgroundColor: '#CCE0EF',
-    },
+    container: { flexGrow: 1, alignItems: 'center', backgroundColor: '#CCE0EF' },
     vTitle: {
         backgroundColor: "#0063B0",
         height: 40,
@@ -144,29 +190,10 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.8,
         shadowRadius: 3,
     },
-    vTitleText: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        paddingLeft: (width / 9) + 5
-    },
-    vTitleIcon: {
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: '#3480BF',
-        width: width / 9,
-        marginRight: 5
-    },
-    title: {
-        color: "white",
-        fontSize: 20
-    },
-    vList: {
-        flex: 1,
-        width: width,
-        alignItems: "center",
-        justifyContent: "center",
-    },
+    vTitleText: { flex: 1, justifyContent: "center", alignItems: "center", paddingLeft: (width / 9) + 5 },
+    vTitleIcon: { justifyContent: "center", alignItems: "center", backgroundColor: '#3480BF', width: width / 9, marginRight: 5 },
+    title: { color: "white", fontSize: 20 },
+    vList: { flex: 1, width: width, alignItems: "center", justifyContent: "center" },
 });
 
 export default Helpdesk;
